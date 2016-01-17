cc585854e529759cb8bad9d80d0ff889d2f70a12
Asgeir Birkisson <birkisson@maths.ox.ac.uk>
2016-01-17 17:19:01 +0000

    Nicer formatting of output.

--- tests/test_all.m
+++ tests/test_all.m
@@ -1,12 +1,47 @@
-sprintf('test_bivariate')
-test_bivariate
-sprintf('test_plotTree')
-test_plotTree
-sprintf('test_printTree')
-test_printTree
-sprintf('test_sortConditions')
-test_sortConditions
-sprintf('test_toFirstOrder')
-test_toFirstOrder
-sprintf('test_univariate')
-test_univariate
+fprintf('test_bivariate      ...  ')
+pass = test_bivariate;
+if (all(pass))
+    fprintf('passed!\n')
+else
+    fprintf('failed!\n')
+end
+
+fprintf('test_plotTree       ...  ')
+pass = test_plotTree;
+if (all(pass))
+    fprintf('passed!\n')
+else
+    fprintf('failed!\n')
+end
+
+fprintf('test_printTree      ...  ')
+pass = test_printTree;
+if (all(pass))
+    fprintf('passed!\n')
+else
+    fprintf('failed!\n')
+end
+
+fprintf('test_sortConditions ...  ')
+pass = test_sortConditions;
+if (all(pass))
+    fprintf('passed!\n')
+else
+    fprintf('failed!\n')
+end
+
+fprintf('test_toFirstOrder   ...  ')
+pass = test_toFirstOrder;
+if (all(pass))
+    fprintf('passed!\n')
+else
+    fprintf('failed!\n')
+end
+
+fprintf('test_univariate     ...  ')
+pass = test_univariate;
+if (all(pass))
+    fprintf('passed!\n')
+else
+    fprintf('failed!\n')
+end
